793a490d175f2021f43e26f11a92f3790af89497
Jeffrey Onochie <jeff.ict@gmail.com>
2021-07-06 14:46:59 +0100

    wire up REDUX DEVTOOL on project

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <React.StrictMode>
