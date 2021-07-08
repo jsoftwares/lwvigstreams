d6fbadd665240978aa6059bd9480f6c0462bf8e2
Jeffrey Onochie <jeff.ict@gmail.com>
2021-07-08 10:26:29 +0100

    create custom browser history object instead of using that created by <BrowserHistory> to handle programmatic navigationf from Action Creator

--- client/src/history.js
+++ client/src/history.js
@@ -3,7 +3,9 @@ import createHistory from 'history/createBrowserHistory';
 /**history package is installed automatically with react-router-dom. It's a separate library that react-router-dom
  * depends upon as a dependency.
  * We use createHistory to create a browser history object that we maintain and can get access to inside our project
- * . This is similar to <BrowserHistory> which maintains a history that keeps track of d paths in our address bar
+ * . This is similar to <BrowserHistory> which maintains a history that keeps track of d paths in our address bar.
+ * When creating/managing you own browser history object, you use a plain router <Router> instead of <BrowserRouter>
+ * which manages it history object itself.
  */
 
 export default createHistory();
